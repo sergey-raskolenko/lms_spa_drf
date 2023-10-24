81a89d768258d99c3dcc9a2ddc83f78bf6486126
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-10-25 01:31:45 +0300

    Added field lessons for CourseSerializer

--- app_course/serializers/course.py
+++ app_course/serializers/course.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 
 from app_course.models import Course
+from app_course.serializers.lesson import LessonSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
 	lesson_counter = serializers.IntegerField(source='lesson_set.all.count', read_only=True)
+	lessons = LessonSerializer(source='lesson_set.all', many=True)
 
 	class Meta:
 		model = Course
