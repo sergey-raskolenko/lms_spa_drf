bba1563b933d590a0b78e99bf1a4a832e9525e77
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-10-25 00:55:56 +0300

    Added Payment model

--- app_payment/models.py
+++ app_payment/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from app_course.models import Course, Lesson
+from config import settings
+from users.models import NULLABLE
 
-# Create your models here.
+
+class Payment(models.Model):
+	PAYMENT_CASH = 'cash'
+	PAYMENT_TRANSFER = 'remittance'
+	PAYMENT_TYPES = [
+		(PAYMENT_CASH, 'наличные'),
+		(PAYMENT_TRANSFER, 'перевод')
+	]
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='пользователь')
+	payment_date = models.DateField(auto_now_add=True, verbose_name='дата оплаты')
+	course = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name='оплаченный курс', **NULLABLE)
+	lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, verbose_name='оплаченный урок', **NULLABLE)
+	amount = models.PositiveIntegerField(verbose_name='сумма оплаты')
+	payment_type = models.CharField(max_length=25, choices=PAYMENT_TYPES, verbose_name='способ оплаты')
+
+	def __str__(self):
+		return f'{self.lesson if self.lesson else self.course}: {self.amount} ({self.amount})'
+
+	class Meta:
+		verbose_name = 'оплата'
+		verbose_name_plural = 'оплаты'
+		ordering = ('payment_date',)
