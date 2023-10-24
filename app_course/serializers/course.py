c8025804875484df84bc1771996d8cef312ed529
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-10-25 00:37:23 +0300

    Added lesson_counter field for course

--- app_course/serializers/course.py
+++ app_course/serializers/course.py
@@ -4,6 +4,8 @@ from app_course.models import Course
 
 
 class CourseSerializer(serializers.ModelSerializer):
+	lesson_counter = serializers.IntegerField(source='lesson_set.all.count')
+
 	class Meta:
 		model = Course
 		fields = '__all__'
