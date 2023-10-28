91b95b2511f13a041d770e1f9c17142039a9fb69
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-10-28 20:38:11 +0300

    Hid password and last name to represent unauthorized users

--- users/views.py
+++ users/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import User
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, UserSerializerForAll
 
 
 class UserViewSet(ModelViewSet):
-	serializer_class = UserSerializer
+	serializer_class = UserSerializerForAll
 	queryset = User.objects.all()
+
+	def retrieve(self, request, *args, **kwargs):
+		instance = self.get_object()
+		if self.kwargs['pk'] == str(request.user.pk):
+			serializer = UserSerializer(instance)
+			return Response(serializer.data)
+		else:
+			serializer = UserSerializerForAll(instance)
+			return Response(serializer.data)
