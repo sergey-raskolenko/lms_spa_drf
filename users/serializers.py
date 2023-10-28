4ed2c08153f4a22bb916be0833e7e7b7021a1209
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-10-28 16:58:19 +0300

    Added field for user payments in UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from app_payment.serializers import PaymentSerializer
 from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+	user_payments = PaymentSerializer(source='payment_set', many=True)
+
 	class Meta:
 		model = User
 		fields = '__all__'
