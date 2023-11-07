28e7c171c99ac671d2cbaf040970ed35490e07cc
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-11-07 23:46:49 +0300

    Simple change

--- config/celery.py
+++ config/celery.py
@@ -6,4 +6,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+app.autodiscover_tasks(['users', 'app_course'])
